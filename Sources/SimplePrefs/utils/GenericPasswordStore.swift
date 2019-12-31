16571309eeb4d586d49a6f11a2212b72d7376790
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2019-12-31 19:46:04 +0100

    _

--- Sources/SimplePrefs/utils/GenericPasswordStore.swift
+++ Sources/SimplePrefs/utils/GenericPasswordStore.swift
@@ -36,11 +36,10 @@ struct GenericPasswordStore {
 	/// Updates a CryptoKit key in the keychain
 	@discardableResult
 	func updateKey(_ key: Data, account: String) -> Bool {
-		
-		// Treat the key data as a generic password.
-		var query = [
-			kSecClass: kSecClassGenericPassword,
-			kSecAttrAccessible: kSecAttrAccessibleWhenUnlocked,
+		deleteKey(account: account)
+		return storeKey(key, account: account)
+		/*let query = [
+			kSecClass: kSecClassGenericPassword
 		] as [String: Any]
 		
 		let attributes = [
@@ -48,13 +47,8 @@ struct GenericPasswordStore {
 			kSecValueData: key
 		] as [String: Any]
 		
-		if #available(OSX 10.15, iOS 13.0, *) {
-			query[kSecUseDataProtectionKeychain as String] = true
-		}
-		
-		// Add the key data.
 		let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
-		return status == errSecSuccess
+		return status == errSecSuccess*/
 	}
 	
 	/// Reads a CryptoKit key from the keychain as a generic password.
